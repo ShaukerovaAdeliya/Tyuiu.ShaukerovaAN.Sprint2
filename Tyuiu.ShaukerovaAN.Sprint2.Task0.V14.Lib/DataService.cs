4bab253fa78a723d4d93119a28a349dedaddaa16
ShaukerovaAdeliya <Tyuiu.ShaukerovaAN@outlook.com>
2023-10-03 20:01:26 +0500

    Add project files.

---
+++ Tyuiu.ShaukerovaAN.Sprint2.Task0.V14.Lib/DataService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using tyuiu.cources.programming.interfaces.Sprint2;
+
+// ЗАДАНИЕ
+// Написать программу из операций сравнений (==, !=, <, >, <=, >=, последовательность операций не должна нарушаться)
+// и арифметических выражений, которая вернет логическую последовательность(массив): (true, false, true, false, true, false),
+// при x = 1075, y = 754
+
+namespace Tyuiu.ShaukerovaAN.Sprint2.Task0.V14.Lib
+{
+    public class DataService : ISprint2Task0V14
+    {
+        public bool[] GetCompareOperations(int x, int y)
+        {
+            bool[] res = new bool[6];
+
+            res[0] = x - 321 == y;
+            res[1] = x - 321 != y;
+            res[2] = x - 1000 < y;
+            res[3] = x - 500 > y;
+            res[4] = x - 500 <= y;
+            res[5] = x - 421 >= y;
+
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.ShaukerovaAN.Sprint2.Task0.V14/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.ShaukerovaAN.Sprint2.Task0.V14.Lib;
+
+// ЗАДАНИЕ
+// Написать программу из операций сравнений (==, !=, <, >, <=, >=, последовательность операций не должна нарушаться)
+// и арифметических выражений, которая вернет логическую последовательность(массив): (true, false, true, false, true, false),
+// при x = 1075, y = 754
+
+
+namespace Tyuiu.ShaukerovaAN.Sprint2.Task0.V14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #2 | Выполнила: Шаукерова А.Н. | ИСТНб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт#1                                                                *");
+            Console.WriteLine("* Тема: Операции сравнения                                                *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #14                                                             *");
+            Console.WriteLine("* Выполнила: Шаукерова Аделия Нуржановна | ИСТНб-23-1                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу из операций сравнений                                *");
+            Console.WriteLine("* (==, !=, <, >, <=, >=, последовательность операций не должна нарушаться)*");
+            Console.WriteLine("* и арифметических выражений, которая вернет логическую                   *");
+            Console.WriteLine("* последовательность(массив):                                             *");
+            Console.WriteLine("*                                                                         *");
+
+            DataService ds = new DataService();
+            int x = 1075;
+            int y = 754;
+            bool[] res = new bool[6];
+            res = ds.GetCompareOperations(x, y);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("X = " + x);
+            Console.WriteLine("Y = " + y);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(res[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
