4bfedd79b48b352a40493ec6738c34c35c2f62ee
ShaukerovaAdeliya <Tyuiu.ShaukerovaAN@outlook.com>
2023-10-13 15:54:25 +0500

    Task6

---
+++ Tyuiu.ShaukerovaAN.Sprint2.Task6.V4.Lib/DataService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.ShaukerovaAN.Sprint2.Task6.V4.Lib
+{
+    public class DataService : ISprint2Task6V4
+    {
+        public string FindCardSuit(int value)
+        {
+            string res;
+            switch (value)
+            {
+                case 1:
+                    res = "Пики";
+                    break;
+                case 2:
+                    res = "Трефы";
+                    break;
+                case 3:
+                    res = "Бубны";
+                    break;
+                case 4:
+                    res = "Червы";
+                    break;
+                default:
+                    throw new ArgumentException($"Игральные карты должны быть от 1 до 4. Значение {value}");
+            }
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.ShaukerovaAN.Sprint2.Task6.V4/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.ShaukerovaAN.Sprint2.Task6.V4.Lib;
+
+namespace Tyuiu.ShaukerovaAN.Sprint2.Task6.V4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #2 | Выполнила: Шаукерова А.Н. | ИСТНб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт#2                                                                *");
+            Console.WriteLine("* Тема: Получение результата из switchФайл                                *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #4                                                              *");
+            Console.WriteLine("* Выполнила: Шаукерова Аделия Нуржановна | ИСТНб-23-1                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая использует сокращенную форму записи         *");
+            Console.WriteLine("* оператора switch вычисляет требуемое значение и возвращает результат.   *");
+            Console.WriteLine("*                                                                         *");
+
+            DataService ds = new DataService();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите номер масти игральной карты: ");
+            int numCard = Convert.ToInt32(Console.ReadLine());
+
+            string res;
+
+            if ((numCard < 1) || (numCard > 4))
+            {
+                res = "Введено неверное значение! ";
+            }
+            else
+            {
+                res = "Масть карты: " + ds.FindCardSuit(numCard);
+            }
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(res);
+
+            Console.ReadKey();
+        }
+    }
+}
